2a09e29add37678c32fc3f1b5029dc84418463bc
juliussaikauskas <juliusaikauskas@gmail.com>
2024-11-17 17:13:11 +0200

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Box, Drawer } from "@mui/material";
+import Router from "./config/Router/Router";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Fuck you</h1>
+    <div>
+      <BrowserRouter>
+        <AppContent />
+      </BrowserRouter>
     </div>
   );
 }
+
+const AppContent: React.FC = () => {
+  const { routes } = Router();
+
+  return (
+    <Box className="flex flex-col h-screen">
+      <Box className="flex flex-grow">
+        <Box>{routes}</Box>
+      </Box>
+    </Box>
+  );
+};
 
 export default App;
