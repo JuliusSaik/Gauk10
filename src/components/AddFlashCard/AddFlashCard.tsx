c629afface64e749d48bdf686db533b0753edd92
juliussaikauskas <juliusaikauskas@gmail.com>
2024-11-30 22:12:54 +0200

    card adding logic and responsive

--- src/components/AddFlashCard/AddFlashCard.tsx
+++ src/components/AddFlashCard/AddFlashCard.tsx
@@ -1,23 +1,61 @@
 import { DeleteRounded, DragHandleRounded } from "@mui/icons-material";
 import { Box, Divider, IconButton, TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { QACard } from "../../pages/CreateStudyCardsPage";
 
-const AddFlashCard = () => {
+export interface AddFlashCardProps {
+  cardId: number;
+  handleChange: (filledCard: QACard) => void;
+  handleDelete: (id: number) => void;
+}
 
-  const [isVisible, setIsVisible] = useState(true);
-
-  const handleDelete = () => {
-    setIsVisible(false);
+const AddFlashCard: React.FC<AddFlashCardProps> = ({
+  cardId,
+  handleChange,
+  handleDelete,
+}) => {
+  const defaultCardValues: QACard = {
+    id: cardId,
+    question: "",
+    answer: "",
   };
+
+  const [cardValues, setCardValues] = useState<QACard>(defaultCardValues);
+
+  const handleQuestionChange = (value: string) => {
+    const filledCard: QACard = {
+      id: cardId,
+      question: value,
+      answer: cardValues.answer,
+    };
+    setCardValues(filledCard);
+  };
+
+  const handleAnswerChange = (value: string) => {
+    const filledCard: QACard = {
+      id: cardId,
+      question: cardValues.question,
+      answer: value,
+    };
+    setCardValues(filledCard);
+  };
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      handleChange(cardValues);
+    }, 500);
+    return () => clearTimeout(timerId);
+  }, [cardValues]);
+
   return (
     <>
       <div className="container mx-auto p-4">
         <Box className="border border-black border-6 bg-gray-800 rounded-lg">
           <div className="flex object-right h-auto flex-line justify-end p-2">
-           
-            <IconButton 
-            aria-label="delete"
-            onClick={handleDelete}>
+            <IconButton
+              aria-label="delete"
+              onClick={() => handleDelete(cardId)}
+            >
               <DeleteRounded className="fill-current text-gray-200 hover:text-blue-500" />
             </IconButton>
           </div>
@@ -28,6 +66,7 @@ const AddFlashCard = () => {
             <div className="flex-1">
               <TextField
                 id="filled-basic"
+                onChange={(e) => handleQuestionChange(e.target.value)}
                 label="Enter question"
                 variant="filled"
                 className="w-full bg-gray-100 text-white p-2 rounded-md outline-none focus:ring-2 focus:ring-blue-500"
@@ -40,6 +79,7 @@ const AddFlashCard = () => {
             <div className="flex-1">
               <TextField
                 id="filled-basic"
+                onChange={(e) => handleAnswerChange(e.target.value)}
                 label="Enter answer"
                 variant="filled"
                 className="w-full bg-gray-100 text-white p-2 rounded-md outline-none focus:ring-2 focus:ring-blue-500"
--- src/pages/CreateStudyCardsPage.tsx
+++ src/pages/CreateStudyCardsPage.tsx
@@ -2,37 +2,70 @@ import { Box, Button } from "@mui/material";
 import React, { useState } from "react";
 import AddFlashCard from "../components/AddFlashCard/AddFlashCard";
 
-export interface QACard{
-  question: string,
-  answer: string,
+export interface QACard {
+  id: number;
+  question: string;
+  answer: string;
 }
 
 const firstCard: QACard = {
-  question: '',
-  answer: '',
-}
-
-
-
+  id: 0,
+  question: "",
+  answer: "",
+};
 
 const CreateStudyCardsPage = () => {
-
   const [createdCards, setCreatedCards] = useState<QACard[]>([firstCard]);
+  const [creationId, setCreationId] = useState(0);
 
+  const onAdd = () => {
+    const incrementId = creationId + 1;
+    setCreationId(incrementId);
+    const createdCard: QACard = {
+      id: incrementId,
+      question: "",
+      answer: "",
+    };
+    setCreatedCards([...createdCards, createdCard]);
+  };
 
-  const onAdd = () => { 
-    setCreatedCards([...createdCards, firstCard]);
-  }
+  const handleCardChange = (filledCard: QACard) => {
+    console.log(createdCards);
+    const modifiedIndex = createdCards.findIndex(
+      (card) => card.id === filledCard.id
+    );
+    const copyOfCards = [...createdCards];
+    copyOfCards[modifiedIndex] = filledCard;
+    setCreatedCards(copyOfCards);
+  };
+
+  const handleDelete = (id: number) => {
+    const deleteIndex = createdCards.findIndex((card) => card.id === id);
+    createdCards.splice(deleteIndex);
+    const copyOfCards = [...createdCards];
+    setCreatedCards(copyOfCards);
+  };
+
+  const onSubmitCards = () => {
+    console.log(createdCards);
+  };
 
   return (
     <>
-    <Button variant="contained" onClick={onAdd} />
+      <Button variant="contained" onClick={onAdd}>
+        Add a card
+      </Button>
+      <Button variant="contained" onClick={onSubmitCards}>
+        Submit
+      </Button>
 
-    {createdCards.map(qaCard => (
-      <AddFlashCard />
-
-    ))}
-    
+      {createdCards.map((qaCard) => (
+        <AddFlashCard
+          cardId={qaCard.id}
+          handleChange={handleCardChange}
+          handleDelete={handleDelete}
+        />
+      ))}
     </>
   );
 };
