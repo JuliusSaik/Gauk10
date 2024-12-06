d084f422bce5602a671a97cd5f47ca0912857f08
Martynas Vasilis <martynas.vasilis@gmail.com>
2024-12-06 20:59:44 +0200

    random

--- src/components/StudyCard/StudyCard.tsx
+++ src/components/StudyCard/StudyCard.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Card } from "./StudyCardList";
 
-export interface Card {
-  title: string;
-  description: string;
-}
-
-const StudyCard: React.FC<Card> = ({ title, description }) => {
-  return <div>{title}</div>;
+const StudyCard: React.FC<Card> = ({ id, question, answer }) => {
+  const [versti, sversti] = useState(false);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
+  return (
+    <div
+      className={`kortele ${versti ? "versti" : ""}`}
+      onClick={() => sversti(!versti)}
+    >
+      <div className="priekis">
+        <p className="text-black">{question}</p>
+      </div>
+      <div className="galas">
+        <p className="text-black">{answer}</p>
+      </div>
+    </div>
+  );
 };
-
 export default StudyCard;
--- src/pages/StudyCardPage.tsx
+++ src/pages/StudyCardPage.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { common } from "@mui/material/colors";
+import React, { useState, useEffect, useRef } from "react";
+import StudyCardList from "C:/Users/marty/gauk10/src/components/StudyCard/StudyCardList";
+import "C:/Users/marty/gauk10/src/components/StudyCard/card.css";
+import { SampleCards } from "./mock/FlashCardQuestions";
 
 const StudyCardPage = () => {
-  return <div>This is where one set of study cards live</div>;
+  const sampleCards = SampleCards;
+  const [cards, setCards] = useState(sampleCards);
+  return (
+    <div className="container">
+      <StudyCardList cards={cards} />
+    </div>
+  );
 };
 
 export default StudyCardPage;
